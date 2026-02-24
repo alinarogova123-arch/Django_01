425a3cb2f0fe28755f6e4cc007aeabb1ab5498c5
alinarogova123-arch <alinarogova123@gmail.com>
2026-02-24 18:05:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
 
-if __name__ == '__main__':
+
+def main():
     passcards = Passcard.objects.all()
     active_passcards = passcards.filter(is_active=True)
-    
-    
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
     print('Активных пропусков:', len(active_passcards))
+
+
+if __name__ == '__main__':
+    main()
