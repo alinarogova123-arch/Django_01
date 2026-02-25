89dc05c4fc69a3350062d708f459f3ab5560b4a8
alinarogova123-arch <alinarogova123@gmail.com>
2026-02-25 20:46:00 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT_NUMBER'],
+        'PORT': os.environ['DB_PORT'],
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER_NAME'],
         'PASSWORD': os.environ['DB_PASSWORD'],
@@ -20,4 +20,5 @@ SECRET_KEY = os.environ['WEBSITE_SECRET_KEY']
 TIME_ZONE = 'Europe/Moscow'
 
 USE_TZ = True
+
 
